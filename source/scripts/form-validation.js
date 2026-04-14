0e5ef1177de46ac276439d82b14453cc2010d193
Vadim <vadimbox85@gmail.com>
2026-04-14 08:46:16 +0300

    Последние проверки линтер + ПП + правка по критериям

--- source/scripts/form-validation.js
+++ source/scripts/form-validation.js
@@ -4,24 +4,21 @@ const phoneInput = form.querySelector('#user-phone');
 const emailInput = form.querySelector('#user-email');
 
 const initFormValidation = () => {
-
   nameInput?.addEventListener('input', () => {
     nameInput.value = nameInput.value.replace(/[^a-zA-Zа-яА-ЯёЁ\s]/g, '');
   });
 
   phoneInput?.addEventListener('input', () => {
-    let value = phoneInput.value.replace(/[^\d+]/g, '');
-    if (value.includes('+')) {
-      value = value[0] === '+'
-        ? '+' + value.slice(1).replace(/\+/g, '')
-        : value.replace(/\+/g, '');
-    }
-    phoneInput.value = value;
+    phoneInput.value = phoneInput.value
+      .replace(/[^\d+]/g, '')
+      .replace(/(?!^)\+/g, '');
   });
 
   emailInput?.addEventListener('input', () => {
-  emailInput.value = emailInput.value.replace(/[^a-zA-Z0-9@._-]/g, '');
-});
+    emailInput.value = emailInput.value
+      .toLowerCase()
+      .replace(/[^a-z0-9@._-]/g, '');
+  });
 };
 
 initFormValidation();
