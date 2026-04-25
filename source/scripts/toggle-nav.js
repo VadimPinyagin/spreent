5c2edb5f30867f5ffb79f89cfedfc7425696903d
VadimPinyagin <vadimbox85@gmail.com>
2026-04-25 14:19:14 +0300

    Меняет header под 320px Меняет имена переменных для шрифтов Переписывает функцию под вынос кнопки из nav Меняет меди-запросы для изображений наmin-width 320

--- source/scripts/toggle-nav.js
+++ source/scripts/toggle-nav.js
@@ -1,5 +1,5 @@
 const nav = document.querySelector('.nav');
-const btnBurger = nav.querySelector('.burger');
+const btnBurger = document.querySelector('.burger');
 const headerButton = nav.querySelector('.header__button');
 const hiddenClass = 'is-hidden';
 const btnCloseClass = 'burger--cross';
@@ -13,18 +13,14 @@ const closeNav = () => {
 };
 
 const toggleNav = () => {
-  nav.addEventListener('click', (e) => {
-    const target = e.target;
-    if (btnBurger.contains(target)) {
-
-      if (nav.classList.contains(hiddenClass)) {
-        nav.classList.remove(hiddenClass);
-        btnBurger.classList.add(btnCloseClass);
-        document.addEventListener('keydown', onEscKeydown);
-        document.addEventListener('click', onOutsideMenuClick);
-      } else{
-        closeNav();
-      }
+  btnBurger.addEventListener('click', () => {
+    if (nav.classList.contains(hiddenClass)) {
+      nav.classList.remove(hiddenClass);
+      btnBurger.classList.add(btnCloseClass);
+      document.addEventListener('keydown', onEscKeydown);
+      document.addEventListener('click', onOutsideMenuClick);
+    } else {
+      closeNav();
     }
   });
 };
@@ -35,8 +31,9 @@ function onEscKeydown(e) {
   }
 }
 
+
 function onOutsideMenuClick(e) {
-  if (!nav.contains(e.target) || headerButton.contains(e.target)) {
+  if ((!nav.contains(e.target) && !btnBurger.contains(e.target)) || headerButton.contains(e.target)) {
     closeNav();
   }
 }
